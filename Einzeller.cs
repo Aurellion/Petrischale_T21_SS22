7b597514acedebbc056fb4c050835beecccb7785
Johannes B <johannes.bock@live.de>
2022-04-25 14:56:44 +0200

    Projektdateien hinzufügen.

---
+++ Einzeller.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Petrischale
+{
+    abstract class Einzeller
+    {
+        public int posX { get; private set; }
+        public int posY { get; private set; }
+
+        static protected Random rnd = new Random();
+
+        Ellipse e = new Ellipse();
+        
+        public Einzeller(Canvas Zeichenfläche, int radius, Brush color)
+        {
+            posX = rnd.Next(0, Convert.ToInt32(Zeichenfläche.ActualWidth));
+            posY = rnd.Next(0, Convert.ToInt32(Zeichenfläche.ActualHeight));
+            e.Width = radius;
+            e.Height = radius;
+            e.Fill = color;
+        }
+
+        public Einzeller(Einzeller ez)
+        {
+            posX = ez.posX;
+            posY = ez.posY;
+            e.Width = ez.e.Width;
+            e.Height = ez.e.Height;
+            e.Fill = ez.e.Fill;
+        }
+
+        public void Draw(Canvas Zeichenfläche)
+        {
+            Zeichenfläche.Children.Add(e);
+            Canvas.SetTop(e,posY);
+            Canvas.SetLeft(e,posX);
+        }
+
+        public void Move()
+        {
+            posX += 2 * rnd.Next(-1, 2);
+            posY += 2 * rnd.Next(-1, 2);
+        }
+
+        public abstract List<Einzeller> Teilen();
+    }
+
+    class Bakterie : Einzeller
+    {
+        public Bakterie(Canvas zeichenfläche) : base(zeichenfläche, 5, Brushes.Green) { }
+        public Bakterie(Bakterie b) : base(b) { }
+        public override List<Einzeller> Teilen()
+        {
+            List<Einzeller> töchter = new List<Einzeller>();
+            if (rnd.NextDouble() < 0.2)
+            {
+                töchter.Add(new Bakterie(this));
+            }
+            return töchter;
+        }
+    }
+
+    class Amöbe : Einzeller
+    {
+        public Amöbe(Canvas zeichenfläche) : base(zeichenfläche, 10, Brushes.Red) { }
+        public Amöbe(Amöbe b) : base(b) { }
+        public override List<Einzeller> Teilen()
+        {
+            List<Einzeller> töchter = new List<Einzeller>();
+            if (rnd.NextDouble() < 0.02)
+            {
+                töchter.Add(new Amöbe(this));
+            }
+            return töchter;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Petrischale
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        List<Einzeller> Zoo = new List<Einzeller>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(17);            
+            timer.Tick += animate;
+        }
+
+        private void animate(object sender, EventArgs e)
+        {
+            Zeichenfläche.Children.Clear();
+            Zoo.ForEach( x => {x.Move(); x.Draw(Zeichenfläche); });
+            List<Einzeller> Kindergarten = new List<Einzeller>();
+            foreach (Einzeller item in Zoo)
+            {
+                Kindergarten.AddRange(item.Teilen());
+            }
+            Zoo.AddRange(Kindergarten);
+        }
+
+        private void btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            for (int i = 0; i < 5; i++)
+            {
+                Zoo.Add(new Bakterie(Zeichenfläche));
+                Zoo.Add(new Amöbe(Zeichenfläche));
+            }
+            
+        }
+    }
+}
